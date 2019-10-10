755bc810d1d29b8bf3c35d8afe3e199682b06b26
Jordan Ou Yang <jordanouyang@server.ca>
2019-10-09 17:12:34 -0700

    added listview to android project

--- InLine2/app/src/main/java/com/example/inline/MainActivity.java
+++ InLine2/app/src/main/java/com/example/inline/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.inline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+    String url ="http://www.google.com";
 
     EditText username;
     EditText password;
@@ -22,12 +34,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
+
+        /*
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url ="http://www.google.com"; //Our endpoint
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);*/
+
         if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
             Toast.makeText(getApplicationContext(),
                     "Redirecting...", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(this, HomeScreenActivity.class);
+            startActivity(intent);
         } else {
             Toast.makeText(getApplicationContext(),
                     "Wrong Credentials..",Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void navRegisterUser(View view) {
+
+        Intent intent = new Intent(this, RegistrationScreen.class);
+        startActivity(intent);
+        // Do something in response to button
+    }
+
 }
