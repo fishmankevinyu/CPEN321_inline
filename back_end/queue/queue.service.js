101aa2ede08e7564ec75eae895a06711e9063758
fishmankevinyu <kaikailongwang3@gmail.com>
2019-10-20 17:48:43 -0700

    complex algorithm wait time

--- back_end/queue/queue.service.js
+++ back_end/queue/queue.service.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Db = require('Mongodb').Db;
-const Server = require('Mongodb').Server
-const MongoClient = require('Mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const Est = require("./estime");
 router.post('/enque',enque);
 router.put('/deque',deque);
