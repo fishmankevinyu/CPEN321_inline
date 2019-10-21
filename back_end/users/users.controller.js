767b35a9269b7c38b82d244ac04ec9758fad735d
fishmankevinyu <kaikailongwang3@gmail.com>
2019-10-20 23:45:01 -0700

    newQueue is added to

--- back_end/users/users.controller.js
+++ back_end/users/users.controller.js
@@ -19,8 +19,8 @@ module.exports = router;
 
 function authenticate(req, res, next) {
     console.log(req.body.registrationToken)
-    User.findOneAndUpdate({userame: req.body.username},{$set: {registrationToken:req.body.registrationToken}});
     userService.authenticate({username: req.body.username , password: req.body.password})
+        .then(User.findOneAndUpdate({userame: req.body.username},{$set: {registrationToken:req.body.registrationToken}})
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
         .catch(err => next(err));
 
