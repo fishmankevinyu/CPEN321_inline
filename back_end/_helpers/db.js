15b8bfde7bd80ea6e787095991f5e294e9187a7d
fishmankevinyu <kaikailongwang3@gmail.com>
2019-11-15 20:37:48 -0800

    move part 1

--- back_end/_helpers/db.js
+++
@@ -1,9 +0,0 @@
-const config = require("../config.json");
-const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-
-module.exports = {
-    user: require("../users/user.model"),
-    course: require("../course/course.model")
-};
