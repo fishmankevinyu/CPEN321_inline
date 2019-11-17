c9a82aed140ba25d3d4016ebd254d33daf8dd92e
fishmankevinyu <kaikailongwang3@gmail.com>
2019-11-16 23:50:37 -0800

    add estime tests

---
+++ back_end/__tests__/estime.spec.js
@@ -0,0 +1,43 @@
+const queue_service = require("../src/queue/queue.service");
+const mock = require("../__mocks__/mock_api_request");
+const Mongodb = require("mongodb").Db;
+const MongoClient = require("mongodb").MongoClient;
+const Mongoose = require("mongoose");
+const config = require("../src/config.json");
+const Db = require("../src/_helpers/db");
+const estime = require("../src/queue/estime");
+
+describe("estime", () =>{
+    var connection;
+    beforeAll(async ()=>{
+        await MongoClient.connect("mongodb://localhost:27017/Est",function(err,_db){
+            if(err) {throw err;}
+            estime.db = _db.db("ESTs");
+            estime.client = _db;
+            estime.ests = estime.db.collection("ests");
+        });
+        connection = await Mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+        Mongoose.Promise = global.Promise;
+    });
+
+    afterAll(async () =>{
+        connection.close();
+        estime.db.close();
+        estime.client.close();
+    });
+
+    test("create estime db instance", ()=>{
+        expect(estime.db).toBeInstanceOf(Mongodb);
+    });
+
+    test("newCourseTime", () =>{
+
+        estime.newCourseTime("ABC000","1").then((x)=>{
+            estime.ests.findOne({coursename: "ABC000"}).then((x)=>{
+                expect(x.coursename).resolves.toBe("ABC000");
+            });
+        });
+
+    });
+
+});
