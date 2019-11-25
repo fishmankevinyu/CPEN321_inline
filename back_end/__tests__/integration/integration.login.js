3d99fc94e12ffb7a7aec65e2926014b03169642a
Danica Xiao <danica@dhcp-206-12-70-170.ubcsecure.wireless.ubc.ca>
2019-11-24 16:39:35 -0800

    log in integration

--- back_end/__tests__/integration/integration.login.js
+++ back_end/__tests__/integration/integration.login.js
@@ -1,10 +1,8 @@
 const config = require("../../src/config.json");
 const mongoose = require("mongoose");
-const userService = require("../../src/users/users.controller");
-const mock = require("../../__mocks__/mock_api_request");
+const userService = require("../../src/users/user.service");
 const User = require("../../src/users/user.model");
-const {MongoClient} = require('mongodb');
-const helper = require("../../src/users/user.service");
+const userController = require("../../src/users/users.controller"); 
 
 const mockRequest = (data) =>{
     return data;
@@ -19,67 +17,77 @@ const mockResponse = () => {
 }
 
 
-describe("login usecase", () =>{
+describe("login", () =>{
          
          
          let connection;
          let connection_;
          let db;
-         var user;
 
          beforeAll(async () => {
-//           await MongoClient.connect("mongodb://localhost:27017",function(err,_db){
-//               if(err) {throw err;}
-//           });
-                   
-        connection = await MongoClient.connect("mongodb://localhost:27017/users", {
-          useNewUrlParser: true,
-        });
-        db = await connection.db(global.MongoClient);
-        MongoClient.Promise = global.Promise;
          
         connection_ = await mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
          //db = await connection.db(global.__MONGO_DB_NAME__);
          mongoose.Promise = global.Promise;
                    
-        user = await User.findOne({username: "lalala"});
-                   
         });
 
-         afterAll(async () => {
-           await connection.close();
-           await connection_.close();
-           await db.close();
-         });
-                  
-    
-        test("login", ()=>{
+         
+  
+    test("create - authenticate - delete", async ()=>{
+     
 
-             const req = mockRequest({body: {
-                                     username: "lalala",
-                                     password: "lalala"
-                                     }
-              });
-              var res = mockResponse();
-              const next = jest.fn();
+        const req_create = mockRequest({body: {
+                                     firstName: "lalala",
+                                     lastName: "hahaha",
+                                     isTeacher: true,
+                                     username: "test3",
+                                     password: "dididi"
+                                     
+              }});
+              var res_create = mockResponse();
+              const next_create = jest.fn();
+              
 
-             const auth = {
-                test(req, res, next){
-                    userService.authenticate(req, res, next);
-                }
+       await userController.register(req_create, res_create, next_create);
 
-             };
-             const spy = jest.spyOn(auth, 'test');
-             auth.test(req, res, next);
 
-             //const spy1 = jest.spyOn(helper, 'create');
-             //spy.mockReturnValue();
+       const req_aut = mockRequest({body: {
+        username: "test3",
+        password: "dididi"
+        
+}});
+    var res_aut = mockResponse();
+    const next_aut = jest.fn();
 
-             expect(spy).toHaveBeenCalledWith(req, res, next);
-             //await expect(spy1).toHaveBeenCalledWith(req.body);
-             expect(res.json).toHaveBeenCalledTimes(1);
-             expect(res.json).toHaveBeenCalledWith(user);
 
-        });
+       await userController.authenticate(req_aut, res_aut, next_aut);
+
+
+
+        var userDel = await User.findOne({username: "test3"}); 
+        await expect(userDel).toBeDefined(); 
+
+       const req_del = mockRequest({params: {id: userDel.id}       
+});
+        var res_del = mockResponse();
+        const next_del = jest.fn();
+       await userController.delete(req_del, res_del, next_del); 
+
+       await expect(res_create.json).toHaveBeenCalledTimes(1); 
+       await expect(res_aut.json).toHaveBeenCalledTimes(1); 
+       var userTest = await User.findOneAndDelete({username: "test3"}); 
+       await expect(userTest).toBe(null); 
+       //await expect(res_del.json).toHaveBeenCalledTimes(1); 
+
+    //    var user = await User.findOne({username: "testtest"}); 
+
+    //    await userService.delete(user.id); 
+
+
+
+   });
+
          
 });
+
